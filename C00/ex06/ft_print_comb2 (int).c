c9e2ab8edcdf26dfdf87505d847d4ee04e64a248
Tom Limon <tolimon@c2r3p9.42perpignan.fr>
2024-07-17 19:36:36 +0200

    fin premier mercredi

--- C00/ex06/ft_print_comb2 (int).c
+++ C00/ex06/ft_print_comb2 (int).c
@@ -6,7 +6,7 @@
 /*   By: tolimon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 16:47:56 by tolimon           #+#    #+#             */
-/*   Updated: 2024/07/17 19:32:46 by tolimon          ###   ########.fr       */
+/*   Updated: 2024/07/17 18:58:06 by tolimon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,37 +15,43 @@
 
 void	ft_print_comb2()
 {
-	char a;
-	char b;
-	char c;
-	char d;
+	char temp;
+	int a;
+	int b;
+	int c;
+	int d;
 
-	a = '0';
-	b = '0';
-	c = '0';
-	d = '0';
-	while(a <= '9') 
+	a = 0;
+	b = 0;
+	c = 0;
+	d = 0;
+	while(a <= 9) 
 	{
 	
-		b = '0';
-		while(b <= '9') 
+		b = 0;	
+		while(b <= 9) 
 		{
+		c = b + 1;
 		
-		
-			c = '0';
-			while(c <= '9')
+			
+			while(c <= 9)
 			{
 			
-				d = '0';		
-				while(d <= '8') 
+				d = c + 1;			
+				while(d <= 9) 
 				{
-					d++;
-					write(1, &a, 1);
-					write(1, &b, 1);
+					temp = a + '0';
+					write(1, &temp, 1);
+					temp = b + '0';
+					write(1, &temp, 1);
 					write(1, " ",1);
-					write(1, &c, 1);
-                                        write(1, &d, 1);
+					temp = c + '0';
+					write(1, &temp, 1);
+					temp = d + '0';
+                                        write(1, &temp, 1);
 					write(1, ", ", 2);
+					
+					d++;
 					
 				}
 				
