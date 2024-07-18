2ec9e691cafa4b1f391acdd1896793d78a1fc967
Tom Limon <tolimon@c2r3p9.42perpignan.fr>
2024-07-18 17:08:01 +0200

    add ex07 C01

--- C00/ex06/ft_print_comb2.c
+++ C00/ex06/ft_print_comb2.c
@@ -6,61 +6,52 @@
 /*   By: tolimon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 16:47:56 by tolimon           #+#    #+#             */
-/*   Updated: 2024/07/17 19:32:46 by tolimon          ###   ########.fr       */
+/*   Updated: 2024/07/18 08:57:13 by tolimon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include <unistd.h>
 
-void	ft_print_comb2()
+void	ft_write_results(a, b, c, d)
 {
-	char a;
-	char b;
-	char c;
-	char d;
-
-	a = '0';
-	b = '0';
-	c = '0';
-	d = '0';
-	while(a <= '9') 
-	{
-	
-		b = '0';
-		while(b <= '9') 
-		{
-		
-		
-			c = '0';
-			while(c <= '9')
-			{
-			
-				d = '0';		
-				while(d <= '8') 
-				{
-					d++;
-					write(1, &a, 1);
-					write(1, &b, 1);
-					write(1, " ",1);
-					write(1, &c, 1);
-                                        write(1, &d, 1);
-					write(1, ", ", 2);
-					
-				}
-				
-				c++;
-			}
-		b++;
-		}
-	a++;
-	}
-
+	write(1, &a, 1);
+	write(1, &b, 1);
+	write(1, " ", 1);
+	write(1, &c, 1);
+	write(1, &d, 1);
 }
 
-
-int main() {
-
-ft_print_comb2();
-
+void	ft_print_comb2(void)
+{
+	char	a;
+	char	b;
+	char	c;
+	char	d;
+	
+	a = '0';
+	while (a <= '9')
+	{	
+		b = '0';
+		while (b <= '9')
+		{	
+			c = a;
+			while (c <= '9')
+			{
+				if (c == a)
+					d = b + 1;
+				else
+					d = '0';
+				while (d <= '9')
+				{			
+					ft_write_results(a, b, c, d);
+					if (!(a == '9' && b == '8' && c == '9' && d == '9'))
+						write(1, ", ", 2);
+					d++;
+				}
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
 }
---
+++ C01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tolimon <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/18 14:24:42 by tolimon           #+#    #+#             */
+/*   Updated: 2024/07/18 16:49:20 by tolimon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int temps;
+
+	
+	int i;
+	i = 0;
+
+	while (i < size / 2)
+	{ 
+		temps = tab[i];
+		tab[i] = tab[size - 1 - i];
+		tab[size - 1 - i] = temps;
+		
+		i++;	
+		
+	}
+
+
+		
+}
+
+int main()
+{
+        int tab[3];
+        int size;
+        size = 3;
+        tab[0] = 1;
+        tab[1] = 2;
+        tab[2] = 3;
+        ft_rev_int_tab(tab, size);
+	printf("%d", tab[2]);
+}
+
