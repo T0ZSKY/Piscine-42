8a41574e8447e6f264803ad117f71317ab48c313
Tom Limon <tolimon@c2r4p7.42perpignan.fr>
2024-07-21 12:13:45 +0200

    rush correct

--- C02/ex07/ft_strupcase.c
+++ C02/ex07/ft_strupcase.c
@@ -6,29 +6,34 @@
 /*   By: tolimon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/21 10:25:25 by tolimon           #+#    #+#             */
-/*   Updated: 2024/07/21 10:48:57 by tolimon          ###   ########.fr       */
+/*   Updated: 2024/07/21 11:07:10 by tolimon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stdio.h>
 
 char	*ft_strupcase(char *str)
 {
 	int	i;
 	int	min;
-	int	maj;
 
+	i = 0;
 	while(str[i])
 	{
 		min = (str[i] >= 'a' && str[i] <= 'z' ); 
 		if(min)
 		{
-			str[i] = str[i] - 32;	
+			str[i] = str[i] - 32;
 		}
+		i++;
 	}
 	return (str);
 }
 
-#include <stdio.h>
-int	main()
+
+int main()
 {
-	ft_strupcase("test");
+	char	src[7] = "test";
+	ft_strupcase(src);
+	printf("%s", src);
+
 }
