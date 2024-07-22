4caa63cbb8318db044936f2cb4e4989b477a0ebd
Tom Limon <tolimon@c2r4p5.42perpignan.fr>
2024-07-22 18:26:59 +0200

    patch ex04 C03

--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: tolimon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 16:54:28 by tolimon           #+#    #+#             */
-/*   Updated: 2024/07/22 17:49:35 by tolimon          ###   ########.fr       */
+/*   Updated: 2024/07/22 18:26:07 by tolimon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,28 +17,23 @@ char	*ft_strstr(char *str, char *to_find)
 
 	i = 0;
 	if (to_find[0] == '\0')
-	{
 		return (str);
-	}
 	j = 0;
 	while (str[i])
 	{
 		if (str[i] == to_find[0])
-        	{
-                j = 0;
-                while (str[i] == to_find[j])
+		{
+			j = 0;
+			while (str[i] == to_find[j])
 			{
-                        	i++;
-                        	j++;
+				i++;
+				j++;
 			}
-                i = i - 2;
-                	if (to_find[j] == '\0')
-                	{
-                        	return (str + i);
-                	}
+			i = i - 2;
+			if (to_find[j] == '\0')
+				return (str + i);
 		}
-         i++;
-
+		i++;
 	}
 	return (0);
 }
