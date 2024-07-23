b4751299bf3dabf1571956525184a99e5a17401d
Tom Limon <tolimon@c2r4p7.42perpignan.fr>
2024-07-23 17:28:19 +0200

    JUSTE POUR LE HORRIBLE ATOY

--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -6,34 +6,45 @@
 /*   By: tolimon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 10:05:49 by tolimon           #+#    #+#             */
-/*   Updated: 2024/07/22 16:35:14 by tolimon          ###   ########.fr       */
+/*   Updated: 2024/07/23 10:15:31 by tolimon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	int				i;
-	unsigned int	a;
-	unsigned int	b;
+	unsigned int	i;
 
 	i = 0;
 	while (s1[i] != '\0' && s2[i] != '\0' && i < n)
 	{
-		a = s1[i];
-		b = s2[i];
-		if (a != b)
-		{
-			return (a - b);
-		}
 		i++;
-	}	
-	return (0);
+	}
+	while (i == n)
+		return (0);	
+	return (s1[i] - s2[i]);
 }
-/*
+#include <string.h>
 #include <stdio.h>
-int     main()
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n);
+
+int	main(void)
 {
-	unsigned int n = 4;
-        printf("%d", ft_strncmp("ateo","ateq", n));
+	char s1[] = "Test1";
+	char s2[] = "OK";
+	char s3[] = "Same";
+	char s4[] = "Size";
+	char s5[] = "Shorter";
+	char s6[] = "ThanMyself";
+	char s7[] = "ShorterTest";
+
+	printf("%d, %d\n", ft_strncmp(s1, s2, 4), strncmp(s1, s2, 4));
+	printf("%d, %d\n", ft_strncmp(s1, s2, 2), strncmp(s1, s2, 2));
+	printf("%d, %d\n", ft_strncmp(s3, s4, 4), strncmp(s3, s4, 4));
+	printf("%d, %d\n", ft_strncmp(s3, s4, 1), strncmp(s3, s4, 1));
+	printf("%d, %d\n", ft_strncmp(s5, s6, 3), strncmp(s5, s6, 3));
+	printf("%d, %d\n", ft_strncmp(s5, s5, 10), strncmp(s5, s5, 10));
+	printf("%d, %d\n", ft_strncmp(s5, s5, 5), strncmp(s5, s5, 5));
+	printf("%d, %d\n", ft_strncmp(s5, s7, 7), strncmp(s5, s7, 7));
+	printf("%d, %d\n", ft_strncmp(s5, s7, 8), strncmp(s5, s7, 8));
 }
-*/
---
+++ C04/ex03/ft_atoi.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tolimon <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/23 14:16:33 by tolimon           #+#    #+#             */
+/*   Updated: 2024/07/23 17:27:39 by tolimon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+void	ft_whitespace(int *i, char *str);
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	j;
+	int	result;
+
+	j = 1;
+	result = 0;
+	i = 0;
+	ft_whitespace(&i, str);
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			j *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + str[i] - '0';
+		i++;
+	}
+	return (result * j);
+}
+
+void	ft_whitespace(int *i, char *str)
+{
+	while (str[*i] == 32 || (str[*i] >= 9 && str[*i] <= 13))
+		*i = *i + 1;
+}
+/*
+#include <stdio.h>
+
+int	ft_atoi(char *str);
+
+int	main(void)
+{
+	printf("42:%d\n", ft_atoi("  \n  42t4457"));
+	printf("-42:%d\n", ft_atoi(" --+-42sfs:f545"));
+	printf("0:%d\n", ft_atoi("\0 1337"));
+	printf("0:%d\n", ft_atoi("-0"));
+	printf("0:%d\n", ft_atoi(" - 1 3 2 5 6 3 2 1 6 7"));
+	printf("-1325632167:%d\n", ft_atoi("-1325632167"));
+	printf("-100:%d\n", ft_atoi("-100"));
+	printf("min:%d\n", ft_atoi("\t---+2147483648"));
+	printf("max:%d\n", ft_atoi("\v2147483647"));
+}
+*/
