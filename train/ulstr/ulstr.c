2b79ff3a1793f6209d5d5690c6a4e6e466043066
Tom Limon <tolimon@c2r3p6.42perpignan.fr>
2024-07-26 11:25:27 +0200

    before exam 2

--- train/ulstr/ulstr.c
+++ train/ulstr/ulstr.c
@@ -5,44 +5,46 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: tolimon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/25 14:09:21 by tolimon           #+#    #+#             */
-/*   Updated: 2024/07/25 14:19:45 by tolimon          ###   ########.fr       */
+/*   Created: 2024/07/26 10:36:07 by tolimon           #+#    #+#             */
+/*   Updated: 2024/07/26 10:43:32 by tolimon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	main(int argc, char *argv[])
+void	ft_ulstr(char *src)
 {
 	int	i;
 
 	i = 0;
+	while(src[i])
+	{
+		if(src[i] >= 'a' && src[i] <= 'z')
+		{
+			src[i] = src[i] - 32;
+			write(1, &src[i], 1);
+		}
 
+		else if(src[i] >= 'A' && src[i] <= 'Z')
+		{
+			src[i] = src[i] + 32;
+			write(1, &src[i], 1);
+		}
+
+		else
+			write(1, &src[i], 1);
+		i++;
+	}
+	
+}
+
+int main(int argc, char *argv[])
+{
 	if(argc != 2)
 	{
 		write(1, "\n", 1);
 		return (0);
 	}
-	
-	while(argv[1][i])
-	{
-		if(argv[1][i] >= 65 && argv[1][i] <= 90)
-		{
-			argv[1][i] = argv[1][i] + 32;
-			write(1, &argv[1][i], 1);
-		}
-
-		else if(argv[1][i] >= 97 && argv[1][i] <= 122)
-		{
-			argv[1][i] = argv[1][i] - 32;
-			write(1, &argv[1][i], 1);
-		}
-		else
-		{
-			write(1, &argv[1][i], 1);
-		}
-		i++;
-
-	}
+	ft_ulstr(argv[1]);
 	write(1, "\n", 1);
 }
