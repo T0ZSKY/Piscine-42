086e7b51fa7c47ba5fa6d4d3725226f18630d1b3
Tom Limon <tomlimon@Macbook-de-Tom.local>
2024-07-29 17:23:48 +0200

    update C07 ft join

--- C07/ex03/ft_strjoin.c
+++ C07/ex03/ft_strjoin.c
@@ -1,90 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tomlimon <tomlimon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/29 17:23:23 by tomlimon          #+#    #+#             */
+/*   Updated: 2024/07/29 17:23:28 by tomlimon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 
 char *ft_strcopy(char *dest, char *src)
 {
-	int i;
-
-	i = 0;
-	while (src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return dest;
+    int i = 0;
+    while (src[i])
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return dest;
 }
 
 int ft_strlen(char *src)
 {
-	int o;
+    int o = 0;
+    while (src[o])
+    {
+        o++;
+    }
+    return o;
+}
 
-	o = 0;
-	while (src[o])
-	{
-		o++;
-	}
-	return o;
+int ft_total_len(int size, char **strs, char *sep)
+{
+    int total_len = 0;
+    int i = 0;
+    int sep_len = ft_strlen(sep);
+
+    while (i < size)
+    {
+        total_len += ft_strlen(strs[i]);
+        i++;
+    }
+    if (size > 1)
+    {
+        total_len += (size - 1) * sep_len;
+    }
+    return total_len + 1; 
 }
 
 char *ft_strjoin(int size, char **strs, char *sep)
 {
-	if (size <= 0)
-		return NULL;
+    if (size <= 0)
+        return NULL;
 
-	int i;
-	int total_len;
-	int sep_len;
-	char *result;
-	int pos;
+    int total_len = ft_total_len(size, strs, sep);
+    char *result = malloc(total_len);
+    if (!result)
+        return NULL;
 
-	i = 0;
-	total_len = 0;
-	sep_len = ft_strlen(sep);
-
-	while (i < size)
-	{
-		total_len = total_len + ft_strlen(strs[i]);
-		i++;
-	}
-
-	if (size > 1)
-		total_len += (size - 1) * sep_len;
-	total_len += 1;
-
-	result = malloc(total_len);
-	if (!result)
-		return NULL;
-
-	i = 0;
-	pos = 0;
-
-	while (i < size)
-	{
-		int len;
-
-		len = ft_strlen(strs[i]);
-		ft_strcopy(result + pos, strs[i]);
-		pos += len;
-
-		if (i < size - 1)
-		{
-			ft_strcopy(result + pos, sep);
-			pos += sep_len;
-		}
-
-		i++;
-	}
-
-	result[pos] = '\0';
-
-	return result;
+    int position = 0;
+    int i = 0;
+    while (i < size)
+    {
+        int len = ft_strlen(strs[i]);
+        ft_strcopy(result + position, strs[i]);
+        position = position + len;
+        if (i < size - 1)
+        {
+            int sep_len = ft_strlen(sep);
+            ft_strcopy(result + position, sep);
+            position = position + sep_len;
+        }
+        i++;
+    }
+    result[position] = '\0';
+    return result;
 }
-#include <stdio.h>
+
 int main()
 {
-	char *strs[] = {"Hello", "World", "42"};
-	char *sep = "_";
-	char *result = ft_strjoin(3, strs, sep);
-	printf("%s\n", result);
-	free(result);
+    char *strs[] = {"Hello", "World", "42"};
+    char *sep = "__";
+    char *result = ft_strjoin(3, strs, sep);
+    printf("%s\n", result);
+    free(result);
+    return 0;
 }
+
+
