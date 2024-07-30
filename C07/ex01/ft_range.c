93781717fd5429aa967ed73fe1e83291d3fe6a97
Tom Limon <tolimon@c2r4p2.42perpignan.fr>
2024-07-30 09:37:01 +0200

    update C07

--- C07/ex01/ft_range.c
+++ C07/ex01/ft_range.c
@@ -6,30 +6,27 @@
 /*   By: tolimon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 10:15:26 by tolimon           #+#    #+#             */
-/*   Updated: 2024/07/29 11:58:52 by tolimon          ###   ########.fr       */
+/*   Updated: 2024/07/30 08:54:05 by tolimon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <unistd.h>
 
-int *ft_range(int min, int max)
+int	*ft_range(int min, int max)
 {
-    int *tab;
-    int i;
+	int	*tab;
+	int	i;
 
-    if (min >= max) 
-        return (NULL);
-
-    tab = malloc(sizeof(int) * (max - min));
-
-    i = 0;
-    while (min < max)
-    {
-        tab[i] = min;
-	min ++;
-	i++;
-    }
-
-    return tab;
+	if (min >= max)
+		return (NULL);
+	tab = malloc(sizeof(int) * (max - min));
+	i = 0;
+	while (min < max)
+	{
+		tab[i] = min;
+		min ++;
+		i++;
+	}
+	return (tab);
 }
--- C07/ex02/ft_ultimete_range.c
+++ C07/ex02/ft_ultimete_range.c
@@ -6,36 +6,30 @@
 /*   By: tolimon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 10:42:07 by tolimon           #+#    #+#             */
-/*   Updated: 2024/07/29 12:05:57 by tolimon          ###   ########.fr       */
+/*   Updated: 2024/07/30 08:57:21 by tolimon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <unistd.h>
 
-int *ft_ultimate_range(int **range, int min, int max)
+int	*ft_ultimate_range(int **range, int min, int max)
 {
-    
-    int i;
+	int	i;
 
-    if (min >= max)
-        return (NULL);
-
-   *range = malloc (sizeof(int) * (max - min));
-
-   if(!range)
-   {
-	return (0);
-   }
-
-
-    i = 0;
-    while (min < max)
-    {
-	(*range)[i] = min;
-	min++;
-        i++;
-    }
-
-    return i; 
+	if (min >= max)
+		return (NULL);
+	*range = malloc (sizeof(int) * (max - min));
+	if (!range)
+	{
+		return (0);
+	}
+	i = 0;
+	while (min < max)
+	{
+		(*range)[i] = min;
+		min++;
+		i++;
+	}
+	return (i);
 }
