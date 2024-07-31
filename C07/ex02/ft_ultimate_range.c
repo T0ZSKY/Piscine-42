3535662dff805b8d59dd1e8c7e001d683abc898b
Tom Limon <tomlimon@Macbook-de-Tom.local>
2024-07-31 17:53:04 +0200

    C07

--- C07/ex02/ft_ultimate_range.c
+++ C07/ex02/ft_ultimate_range.c
@@ -1,38 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ultimete_range.c                                :+:      :+:    :+:   */
+/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tolimon <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: tomlimon <tomlimon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/29 10:42:07 by tolimon           #+#    #+#             */
-/*   Updated: 2024/07/31 17:33:35 by tolimon          ###   ########.fr       */
+/*   Created: 2024/07/31 17:38:16 by tomlimon          #+#    #+#             */
+/*   Updated: 2024/07/31 17:52:22 by tomlimon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <unistd.h>
 
 int	ft_ultimate_range(int **range, int min, int max)
 {
 	int	i;
 
+	i = 0;
 	if (min >= max)
 	{
 		range = NULL;
 		return (0);
 	}
-	*range = malloc (sizeof(int) * (max - min));
-	if (!range)
-	{
-		return (-1);
-	}
-	i = 0;
-	while (min != max)
+	*range = malloc(sizeof(int) * (max - min));
+	while(min != max)
 	{
 		(*range)[i] = min;
 		min++;
 		i++;
+	}
+	if (range == NULL)
+	{
+		return (-1);
 	}
 	return (i);
 }
